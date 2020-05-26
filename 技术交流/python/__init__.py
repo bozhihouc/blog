d536d4b40bae15a365ec07d77cec5451d22a334b
Administrator <852782749>
2020-05-26 16:08:20 +0800

    2020年5月26号更新

---
+++ 技术交流/python/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020年5月26日, 0026 14:07
+# @Author  : 862897316@qq.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+import requests
+import json
---
+++ 技术交流/python/修改文件.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020年5月26日, 0026 14:07
+# @Author  : 862897316@qq.com
+# @File    : 修改文件.py
+# @Software: PyCharm
+
+import re
+import time
+
+def xieru():
+    old_file = '1.html'
+    fopen = open(old_file, 'r')
+    old_str = '<script language="JavaScript" src="123.js"></script>'
+    print(old_str)
+    w_str = ""
+    new_str = '<script language="JavaScript" src="123.js"></script>\n<script language="JavaScript" src="http://www.baidu.com/123.js"></script>'
+    for line in fopen:
+        if re.search('baidu', line):
+            print('jhhh')
+        else:
+            line = re.sub(old_str, new_str, line)
+            w_str += line
+    print(w_str)
+    wopen = open(old_file, 'w')
+    wopen.write(w_str)
+    wopen.close()
+    fopen.close()
+if __name__ == '__main__':
+    for i in range(99):
+        xieru()
+        time.sleep(19)
